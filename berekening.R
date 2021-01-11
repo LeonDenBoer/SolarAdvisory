f595d598ff10a81293096c9f9359f8c007af07a6
leondenboer <Boerleo@gmail.com>
2021-01-11 14:09:18 +0100

    Update berekening.R

--- berekening.R
+++ berekening.R
@@ -9,36 +9,38 @@ personen = c(1,2,3,4,5)
 KwU = c(1850, 2860, 3400, 3930, 4180)
 huishouden <- data.frame(personen, KwU)
 
+panelen = c(11,16,20,22,25,26)
+kosten_p = c(5100,7600,9100,10200,11700,12000)
+zon_kos = data.frame(panelen,kosten_p)
+
+
 
 #bereken het kilowatt per uur aan dat een huis kan regelen
-calc_kwu <- function(pc, date) {
+calc_kwu <- function(pc, zonnepanelen, date) {
   huis <- utrecht[utrecht$postcode == pc,]
-  print(huis)
   
   if(huis$woningtype != "Flat"){#Flat betekend meestal dat je zelf geen huis eigenaar bent en geen recht hebt
     
-    if(huis$hellingshoek != 0){ #platdak heeft meer ruimte nodig voor de zonnepanelen
-      zonnepanelen <- floor(huis$oppervlakte / 1.5)
-    }else{
+
     
-      zonnepanelen <- floor(huis$oppervlakte / 2.5)
-   
-    }
-    
+  
       secondes <- as.numeric((as.POSIXct(date, origin = "1970-01-01")))
-      cm_oppervlakte <- zonnepanelen * 1000
+      oppvervlakte <- zonnepanelen * 1.63
+      cm_oppervlakte <- oppvervlakte * 1000
       year_joule <- cm_oppervlakte * predict(modelFit, data.frame(secondes = secondes)) * 365
+      
       kw <- year_joule / 3600000
-      column <- select(opbrengt_ori, toString(round_any(huis$ori.ntatie, 5)))
+      column <- select(opbrengt_ori, toString(round_any(huis$ori.ntatie, 10)))
       row <- which(opbrengt_ori$hellingshoek == round_any(huis$hellingshoek, 10))
       kwu <- kw * (column[row,] /100)
+      print(zonnepanelen)
     
   }else(
     kwu <- 0
   )
   print("1")
-  print(kwu)
   return(kwu)
+
   
 }
 
@@ -49,7 +51,7 @@ calc_kwu <- function(pc, date) {
 calc_rendement <- function(postcode, pp){
   Date <- Sys.Date()
   huis <- utrecht[utrecht$postcode == postcode,]
-
+  zonnepanelen <- calc_zonnepanelen(huis, pp)
   if(pp < 20){
     if(pp > 5){pp <- 5}
     huishoud <- huishouden[huishouden$personen == pp,]
@@ -58,12 +60,13 @@ calc_rendement <- function(postcode, pp){
     energie <- pp
   }
   #voor het moment gaan we uit van 240 euro per m2 en 600 installatie kosten
-  kosten <- huis$oppervlakte * 240 + 600
+  kosten <- zon_kos[which.min(abs(zonnepanelen-zon_kos$panelen)),]$kosten
   verdienen <- 0
   jaren_bezig <- 0
+  print("kosten")
   print(kosten)
   repeat {
-    year_kwu <- calc_kwu(postcode, as.Date(Date)+jaren_bezig)
+    year_kwu <- calc_kwu(postcode, zonnepanelen, as.Date(Date)+jaren_bezig)
     print(year_kwu)
     print("2")
     print(energie)
@@ -78,8 +81,10 @@ calc_rendement <- function(postcode, pp){
       print(earning)
       print(standaard_kosten)
       print(verdienen)
+      
     }else{
       verdienen <- verdienen + (year_kwu * 0.22)
+      
     }
     
     
@@ -90,7 +95,48 @@ calc_rendement <- function(postcode, pp){
     print(jaren_bezig/365)
     if (kosten < verdienen) break
   }
-   print(jaren_bezig/365)
+  
+  co2 <- year_kwu* 0.649
+  print(co2)
+  print(jaren_bezig/365)
   
   
 }
+
+calc_zonnepanelen <- function(huis, pp){
+  kwu <- 1000000
+  
+  if(pp < 20){
+    if(pp > 5){pp <- 5}
+    huishoud <- huishouden[huishouden$personen == pp,]
+    personen_kwh <- as.integer(huishoud$KwU)
+  }else{
+    personen_kwh <- pp
+  }
+  if(huis$hellingshoek != 0){ #platdak heeft meer ruimte nodig voor de zonnepanelen
+    zonnepanelen <- floor(huis$oppervlakte / 1.63)
+  }else{
+  
+    zonnepanelen <- floor(huis$oppervlakte / 2.5)
+  
+  }
+  
+  zonnepanelen <- zonnepanelen + 1
+  rotaties <- 0
+  repeat{
+    rotaties <- rotaties + 1
+   
+    if (kwu < personen_kwh) break
+    
+    
+    kwu <- calc_kwu(huis$postcode, zonnepanelen, as.Date(Sys.Date()))
+    print(zonnepanelen)
+    zonnepanelen <- zonnepanelen - 1
+  }
+  if(rotaties = 1)
+  
+  return(zonnepanelen)
+}
+
+
+
